1d6ef75933d6024dee0c79dd4a6379fc9e8bb15a
Jian Sun <slgsunjian@gmail.com>
2021-12-18 22:37:38 +0800

    add vector constructor unit test

--- wheel_stl/vector/test_official_vector.cpp
+++ wheel_stl/vector/test_official_vector.cpp
@@ -1,23 +1,67 @@
+// https://www.cplusplus.com/reference/vector/vector/
+
 #include <gtest/gtest.h>
 #include <vector>
 
-TEST(Vector, empty_container_constructor) {
-  std::vector<int> vec;
-  EXPECT_EQ(0, vec.size());
-}
-
-TEST(Vector, fill_constructor) {
+TEST(Vector, constructor) {
+  // empty container constructor
   {
-    std::vector<int> vec(5);  // 5 elements
-    EXPECT_EQ(5, vec.size());
+    std::vector<int> vec;
+    EXPECT_EQ(0, vec.size());
+  }
+
+  // fill constructor 1
+  {
+    const int kElementNum = 5;  // 5 elements
+    std::vector<int> vec(kElementNum);
+    EXPECT_EQ(kElementNum, vec.size());
   }
 
   {
-    std::vector<int> vec(3, 100);  // 3 ints with value 100
-    EXPECT_EQ(3, vec.size());
+    // fill constructor 2
+    const int kElementNum = 3;  // 3 elements
+    const int kValue = 100;     // with value 100
 
-    for (int i = 0; i < vec.size(); ++i) {
-      EXPECT_EQ(100, vec[i]);
+    std::vector<int> fill_vec(kElementNum, kValue);
+
+    EXPECT_EQ(kElementNum, fill_vec.size());
+    for (size_t i = 0; i < fill_vec.size(); ++i) {
+      EXPECT_EQ(kValue, fill_vec[i]);
+    }
+
+    // range constructor
+    std::vector<int> range_vec(fill_vec.begin(), fill_vec.end());
+
+    EXPECT_EQ(kElementNum, range_vec.size());
+    for (size_t i = 0; i < range_vec.size(); ++i) {
+      EXPECT_EQ(kValue, range_vec[i]);
+    }
+
+    // copy constructor
+    std::vector<int> copy_vec(range_vec);
+    EXPECT_EQ(kElementNum, copy_vec.size());
+    for (size_t i = 0; i < copy_vec.size(); ++i) {
+      EXPECT_EQ(kValue, copy_vec[i]);
+    }
+
+    // move constructor (C++11)
+    std::vector<int> move_rec(std::move(copy_vec));
+    EXPECT_EQ(kElementNum, move_rec.size());
+    for (size_t i = 0; i < move_rec.size(); ++i) {
+      EXPECT_EQ(kValue, move_rec[i]);
+    }
+    // after move, copy_vec is in an unspecified but valid state.
+    // https://godbolt.org/z/GMMM7Pfhf
+    EXPECT_EQ(0, copy_vec.size());
+    EXPECT_EQ(true, copy_vec.empty());
+  }
+
+  {
+    // initializer list (C++11)
+    std::vector<int> list_vec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    EXPECT_EQ(10, list_vec.size());
+    for (size_t i = 0; i < list_vec.size(); ++i) {
+      EXPECT_EQ(i, list_vec[i]);
     }
   }
 }
