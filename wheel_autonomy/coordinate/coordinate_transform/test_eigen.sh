f52263976c03d1aae130ad1a2d5675963987ca42
Jian Sun <slgsunjian@gmail.com>
2022-05-03 11:58:47 +0800

    move file position

---
+++ wheel_autonomy/coordinate/coordinate_transform/test_eigen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bazel test //wheel_autonomy/coordinate/coordinate_transform:eigen_basic_usage --test_output=all
--- wheel_autonomy/coordinate_transform/test_eigen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bazel test //wheel_autonomy/coordinate_transform:eigen_basic_usage --test_output=all
