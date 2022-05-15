6996d067c601c1b04d3f2b97ab4286ea690330f7
Jian Sun <slgsunjian@gmail.com>
2022-05-15 11:18:28 +0800

    add code coverage command

--- utilities/build_all.sh
+++ utilities/build_all.sh
@@ -15,3 +15,7 @@ bazel build ... --disk_cache=~/.wheel_stl_bazel_cache
 
 # run uint test
 bazel test ... --test_output=all
+
+# code coverage
+bazel coverage --instrument_test_targets --experimental_cc_coverage --combined_report=lcov --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main  //wheel_autonomy/transformations/geographic_coordinate/...
+genhtml --output genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
