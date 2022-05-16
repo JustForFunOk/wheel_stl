81c11b8117a770ad99bfc4ba03afd094bafc4255
Jian Sun <slgsunjian@gmail.com>
2022-05-16 22:03:36 +0800

    refine build script

--- utilities/build_all.sh
+++ utilities/build_all.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+function bazel_build() {
+  # build all
+  bazel build ... --disk_cache=~/.wheel_stl_bazel_cache
+}
+
+function unit_test() {
+  # run unit test
+  bazel test ... --test_output=all
+}
+
+function code_coverage() {
+  echo "\n----------------------code coverage---------------------"
+
+  # code coverage
+  # 1
+  # WARNING: Option 'javabase' is deprecated  (bazel version 5.1.1)
+  # --javabase=@bazel_tools//tools/jdk:remote_jdk11 \
+  # 2
+  # macos use llvm which does not support gcov
+  # https://stackoverflow.com/questions/7988355/getting-0-gcov-coverage-for-a-mac-app
+  # --experimental_use_llvm_covmap \
+  bazel coverage \
+  --instrument_test_targets \
+  --experimental_cc_coverage \
+  --combined_report=lcov \
+  --coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main \
+  --java_runtime_version=remotejdk_11 \
+  //...
+
+  # generate html to ./bazel-out/code_coverage folder
+  genhtml --output ./bazel-out/code_coverage "$(bazel info output_path)/_coverage/_coverage_report.dat"
+}
+
+
 # get the root path of git repository
 repo_path=$(git rev-parse --show-toplevel)
 
@@ -10,22 +44,15 @@ echo "Current Path: $(pwd)"
 # Check code style
 ./utilities/format_code.sh
 
-# build all
-bazel build ... --disk_cache=~/.wheel_stl_bazel_cache
+# build
+bazel_build
 
-# run uint test
-bazel test ... --test_output=all
+# test
+unit_test
 
 # code coverage
-# WARNING: Option 'javabase' is deprecated  (bazel version 5.1.1)
-# --javabase=@bazel_tools//tools/jdk:remote_jdk11 \
-bazel coverage \
---instrument_test_targets \
---experimental_cc_coverage \
---combined_report=lcov \
---coverage_report_generator=@bazel_tools//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:Main \
---java_runtime_version=remotejdk_11 \
-//...
-
-# generate html to ./bazel-out/code_coverage folder
-genhtml --output ./bazel-out/code_coverage "$(bazel info output_path)/_coverage/_coverage_report.dat"
+if [[ "$(uname -s)" == "Darwin"* ]]; then
+  echo "[code coverage] does not work no macos, skip..."
+else
+  code_coverage
+fi
