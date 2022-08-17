c7a25f496aee272ff4590ea0dcc5439c8e0f1038
Jian Sun <slgsunjian@gmail.com>
2022-08-17 23:52:53 +0800

    Add some member function for array

--- wheel_stl/array/wheelstl_array.h
+++ wheel_stl/array/wheelstl_array.h
@@ -3,6 +3,13 @@
 /*
 why add const?
 warning: 'constexpr' non-static member function will not be implicitly 'const' in C++14; add 'const' to avoid a change in behavior [-Wconstexpr-not-const]
+
+noexcept
+at()函数由于会检查是否越界，会抛出异常，所以不能用noexcept修饰
+
+constexpr
+array size()在编译期间就能计算出来，所以用constexpr修改，减少运行时间
+
 */
 
 
@@ -20,7 +27,7 @@ class array {
     typedef _Tp*                iterator;
     typedef const _Tp*          const_iterator;
 
-    tyoedef std::size_t           size_type;
+    typedef std::size_t         size_type;
 
 
     // Iterators
@@ -30,14 +37,14 @@ class array {
     /**
      * @brief Return the number of elements in the array container.
      */
-    constexpr std::size_t size() const noexcept {
+    constexpr size_type size() const noexcept {
         return _Nm;
     }
 
     /**
      * @brief Return the maximum number of elements that the array container can hold.
      */
-    constexpr std::size_t max_size() const noexcept {
+    constexpr size_type max_size() const noexcept {
         return _Nm;
     }
 
@@ -50,17 +57,47 @@ class array {
 
     //Element access
     /**
-     * @brief 
+     * @brief Returns a reference to the element at position n in the array container.
      * 
+     * @param n Position of an element in the array. Start from 0.
+     * @return reference The element at the specified position in the array.
      */
-    _Tp& operator[](std::size_t n) {
-        return data[n];
+    reference operator[](size_type n) noexcept {
+        return data_[n];
+    }
+
+    /**
+     * @brief If the array object is const-qualified, the function returns a const_reference.
+     * 如何调用到这个函数？
+     * 
+     * @overload const 构成重载？
+     * @param n 
+     * @return const_reference 
+     */
+    const_reference operator[](size_type n) const noexcept {
+        return data_[n];
+    }
+
+    /**
+     * @brief Return a reference to the element at the position n in the array.
+     * 
+     * Contrast with member operator[], this function checks whether n is within the bounds of valid elements in the container.
+     * 
+     * @param n Position of an element in the array. Start from 0.
+     * @return reference The element at the specified position in the array.
+     */
+    reference at(size_type n) noexcept {
+        return data_[n];
+    }
+
+    const_reference at(size_type n) const noexcept {
+        return data_[n];
     }
 
     // Modifiers
 
   private:
-    _Tp data[_Nm];
+    _Tp data_[_Nm];
 };
 
 }  // end of namespace wheelstl
